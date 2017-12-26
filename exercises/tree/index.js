8fe42085304ea771bb1adeba63227611db057ec1
Keith Rodriguez <rodriguez.keith@gmail.com>
2017-12-26 16:05:01 -0600

    Tree Traversal

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,48 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    // remove the references to nodes that are equal to the input data, keep those that do not
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while(arr.length) {
+      const node = arr.shift(); // remove next node
+      
+      arr.push(...node.children); // add its children
+      fn(node); // apply function to removed node
+    }
+  }
+  
+  traverseDF(fn) {
+    const arr = [this.root];
+    while(arr.length) {
+      const node = arr.shift();
+
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
