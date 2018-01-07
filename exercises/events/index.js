762a849bc5fbdf6c10d1d746211579ffe7c06198
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-01-06 19:53:37 -0500

    Events, bubble sort, selection sort

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,36 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(this.events[eventName]) {
+      // if there is this event, add the callback to the array
+      this.events[eventName].push(callback);
+    } else {
+      // else add a new array with the callback in it
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if(this.events[eventName]) {
+      for(let cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
